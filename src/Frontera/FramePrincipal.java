12bc3b1e8be8e6ca4c6e066c0626d4a53f10a938
Nicolas Hoyos <49196666+nhoyosp@users.noreply.github.com>
2020-10-29 09:58:26 -0500

    Laboratorio 2 de ingesoft realizado

--- src/Frontera/FramePrincipal.java
+++ src/Frontera/FramePrincipal.java
@@ -83,7 +83,7 @@ public class FramePrincipal extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(panelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(panelPrincipal, javax.swing.GroupLayout.DEFAULT_SIZE, 244, Short.MAX_VALUE)
                 .addContainerGap())
         );
 
